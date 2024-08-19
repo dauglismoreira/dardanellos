3d9b4d05036adbddd6866808966f89a037c033c6
Dauglis Moreira <dauglisc@gmail.com>
2024-08-19 18:27:02 -0300

    fix banner subtitle

--- app/components/ui/homeOppBanner/index.tsx
+++ app/components/ui/homeOppBanner/index.tsx
@@ -10,8 +10,8 @@ export const HomeOppBanner = ({data}: HomeOppBannerProps) => {
     return(
         <div className="home-opp-banner" style={{backgroundImage: `url('${data.horizontal_image_id ? getStorageFile(data.horizontal_image.src) : '/placeholder-dark.jpg'}')`}}>
             <div className="home-opp-banner-content">
-                <h3>{data.long_text_pt_br}</h3>
-                <PrimaryButton label={data.link_label_pt_br} link={data.link} style="border-dardaGray1 text-dardaGray1 hover:border-transparent hover:bg-darda5"/>
+                <h3>{data.subtitle_pt_br}</h3>
+                <PrimaryButton label={data.link_label_pt_br || ''} link={data.link || ''} style="border-dardaGray1 text-dardaGray1 hover:border-transparent hover:bg-darda5"/>
             </div>
         </div>
     )
