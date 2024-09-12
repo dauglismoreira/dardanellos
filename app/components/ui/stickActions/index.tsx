28d8c0e21a701df92ec7d9e923f33aab925c7339
buenexx <buenofernando2013@gmail.com>
2024-09-12 11:36:38 -0300

    update whats

--- app/components/ui/stickActions/index.tsx
+++ app/components/ui/stickActions/index.tsx
@@ -1,27 +1,25 @@
 'use client';
 
-import { IoLogoWhatsapp } from 'react-icons/io5';
+import {IoLogoWhatsapp} from 'react-icons/io5';
 import './styles.css';
-import { MdOutlineMail } from 'react-icons/md';
-import { BsChatLeftText } from 'react-icons/bs';
+import {MdOutlineMail} from 'react-icons/md';
+import {BsChatLeftText} from 'react-icons/bs';
 import Link from 'next/link';
+import LinkWhats from "@/app/components/ui/chatWhatsapp/Link";
 
 export const StickActions = ({data}:any) => {
 
     const phone = data.configs.find((config:any) => config.id === 5).phone;
-    const cleanedPhone = phone.replace(/\D/g, '');
-
-    const whatsappLink = `https://wa.me/55${cleanedPhone}`;
 
     const att = data.configs.find((config:any) => config.id === 7).link;
     const email = data.configs.find((config:any) => config.id === 6).email;
     
     return(
         <div className="stick-actions">
-            <Link href={whatsappLink} target="_blank"><div className="stick-option">
+            <LinkWhats phone={phone}><div className="stick-option">
                 <IoLogoWhatsapp />
                 <span>Whatsapp</span>
-            </div></Link>
+            </div></LinkWhats>
             <Link href={`mailto:${email}`}><div className="stick-option">
                 <MdOutlineMail />
                 <span>Mensagem</span>
